e1199f7d89b5f81b5f744ce1418516a322e1bd55
roberts111246 <44642949+roberts111246@users.noreply.github.com>
2018-11-26 08:53:48 -0600

    update all

--- rocketglasses/main.js
+++ rocketglasses/main.js
@@ -1,8 +1,7 @@
 function main() {
     $('.infocol').on('click', function() {
+        $('.infocol, .infotxt, #overlay').toggle(); 
         $(this).toggleClass('activeinfocol');
-        $('.infocol:not(.activeinfocol)').toggle(); //messier line of code; essentially affects every infocol that isn't the clicked one
-        $('#productimg').toggle();
         $(this).show(); 
     });
 }
