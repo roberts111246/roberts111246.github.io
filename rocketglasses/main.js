2fc6855bd279869c861b46110a114c0462b5c8e1
roberts111246 <44642949+roberts111246@users.noreply.github.com>
2018-12-04 08:53:53 -0600

    update all

--- rocketglasses/main.js
+++ rocketglasses/main.js
@@ -6,11 +6,15 @@ function main() {
         $(this).addClass('activeinfocol');
         $(this).removeClass('infocol');
         $(this).show(); //only shows infocol, can't do anything else
+        $('.infocolexit').show();
     });
     
     $('.infocolexit').click(function() {
         $('.infotxt, #overlay').hide();
         $('.infocol').show();
+        $('.activeinfocol').addClass('infocol');
+        $('.activeinfocol').removeClass('activeinfocol');
+        $('.infocolexit').hide();
     });
     
 }
